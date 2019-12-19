bd119569577aa89079e6bc9a560b44fd885a33fb
Lacrotte Florent <lacrotte.florent@gmail.com>
2019-12-19 15:26:41 +0100

    maquetage HTML CSS section 1 et 2 + entête et pied de page

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE HTML>
+<!-- Couleurs :
+    Blanc       #FFFFFF
+    Gris        #333
+    Gris clair  #919191
+    Gris foncé  #181A1B
+    Orangé      #542E00
+    Bleu clair  #00A3D0
+    Bleu gris   #38435A
+    Bleu foncé  #181D27
+    -->
+<!-- Blog d'écrivain - Made by Lacrotte Florent / Projet 4 - OpenClassrooms -->
+<html>
+    
+      <head>
+        <meta charset="utf-8">
+        <!-- Viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- "shrink-to-fit=no" empèche safari d'adapter le contenu débordant à la taille de la fenêtre -->
+        <!-- Indexation robot goolge SEO -->
+        <meta name="robots" content="index, follow" />
+        <!-- Media Queries -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" /> <!-- Bootstrap -->
+        <link rel="stylesheet" href="./css/style.css" />
+        <link rel="stylesheet" media="(max-width:768.00px)" href="./css/small.css" />
+        <link rel="stylesheet" media="(min-width:768.00px) and (max-width:975.00px)" href="./css/medium.css" />
+        <link rel="stylesheet" media="(min-width:975.00px)" href="./css/large.css" />
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800|Source+Sans+Pro:300,400,900|Roboto&display=swap" rel="stylesheet">
+        <!-- Fav Icon -->
+        <link rel="icon" href="" />  <!-- A COMPLETER -->
+        <!-- Titre -->
+        <title>JEAN FORTEROCHE</title>
+        <!-- Auteur -->
+        <meta name="author" content="Lacrotte Florent" />
+        <!-- Meta descritpion -->
+        <meta name="description" content="Le livre en ligne de JEAN FORTEROCHE" /> 
+        <!-- Meta Facebook -->
+        <!-- Open Graph data -->
+        <meta property="og:title" content="JEAN FORTEROCHE" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="" />  <!-- A COMPLETER -->
+        <meta property="og:image" content="" /> <!-- A COMPLETER -->
+        <meta property="og:locale" content="fr_FR" />
+        <meta property="og:description" content="Le livre en ligne de JEAN FORTEROCHE" />
+        <!-- Meta Twitter -->
+        <!-- Twitter Card data -->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@JFORTEROCHE" />
+        <meta name="twitter:title" content="JEAN FORTEROCHE" />
+        <meta name="twitter:description" content="Le livre en ligne de JEAN FORTEROCHE" />
+        <meta name="twitter:creator" content="@LacrotteFlorent" />
+        <meta name="twitter:image" content="" />  <!-- A COMPLETER -->
+        <!-- Compatibilité IE8 -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+      </head>
+    
+    <body>
+        <header class="col-lg-12">
+            <div class="headband">
+                <div class="row">
+                    <h1 class="titleFirstPart col-12">JEAN</h1>
+                    <h1 class="titleSecPart col-12">FORTEROCHE</h1>
+                    <div class="splitter col-12">
+                        <div class="row">
+                            <div class="spaceSplit col-1"></div>
+                            <div class="split col-10"></div>
+                            <div class="spaceSplit col-1"></div>
+                        </div>
+                    </div>
+                    <nav class="col-12">ACCUEIL | BLOG | CONTACT</nav>
+                    <div class="socialNetwork col-12">
+                        <div id="facebook" class="circle">
+                        </div>
+                        <div id="instagram" class="circle">
+                        </div>
+                        <div id="twitter" class="circle">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <section id="topLeaf">
+            <div class="row">
+                <div id="spaceLeaf" class="col-2"></div>
+                <div id="photoLeaf" class="col-8">
+                    <div id="frameworkLeaf">
+                        <div id="textLeaf">
+                            <h2>"Billet simple pour l'Alaska"</h2>
+                            <p>Un roman publié en ligne</p>
+                        </div>
+                        <div id="inputLeaf">
+                            <button>Dernier Chapitres</button>
+                            <button>A Propos</button>
+                            <button>Livre</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="spaceLeaf col-2"></div>
+            </div>
+        </section>
+
+        <section id="insert">
+            <p><i>"J'ai fait le choix de partager ce livre avec vous en m'affranchissant d'un éditeur classique"</i></p>
+            <p><i>Partagez mon aventure !</i></p>
+            <div class="socialNetwork col-12">
+                <div id="facebook" class="circle">
+                </div>
+                <div id="instagram" class="circle">
+                </div>
+                <div id="twitter" class="circle">
+                </div>
+            </div>
+        </section>
+
+        <section id="news">
+            <h2>Derniers Chapitres</h2>
+            <div class="splitter col-12">
+                <div class="row">
+                    <div class="spaceSplit col-1"></div>
+                    <div class="split col-10"></div>
+                    <div class="spaceSplit col-1"></div>
+                </div>
+            </div>
+            <div id=frameworkNews class="row">
+                <div class="chapter col-4">
+                    <div class="row">
+                        <div class="spaceChapter col-1"></div>
+                        <div class="photoChapter col-10">
+                            <img src="img/chapter1.jpg" />
+                        </div>
+                        <div class="spaceChapter col-1"></div>    
+                    </div>
+                    <div class="context">
+                        <h3>INSERER TITRE ICI</h3>
+                        <p class="dateChapter">34 décembre 2393</p>
+                        <p class="contentChapter">Salut, il était une fois une jolie princesse au royaumme des neiges qui rencontre un gentil bonhomme de neige Olaf</p>
+                    </div>
+                    <div class="more">
+                        <button>Lire la suite</button>
+                    </div>
+                    <div class="socialNetworkChapter row">
+                        <div class="split col-3"></div>
+                        <div class="socialCircles col-6">
+                            <div class="row">
+                                <div id="facebook" class="circle"></div>
+                                <div id="instagram" class="circle"></div>
+                                <div id="twitter" class="circle"></div>
+                            </div>
+                        </div>
+                        <div class="split col-3"></div>
+
+                    </div>
+                </div>
+                <div class="chapter col-4">
+                
+                </div>
+                <div class="chapter col-4">
+                
+                </div>
+            </div>
+        </section>
+
+        <section id="about">
+            <h2>A propos</h2>
+            <div class="splitter col-12">
+                <div class="row">
+                    <div class="spaceSplit col-1"></div>
+                    <div class="split col-10"></div>
+                    <div class="spaceSplit col-1"></div>
+                </div>
+            </div>
+            
+        </section>
+        
+        
+        <footer class="row">
+            <div class="footband col-12">
+                <div class="row">
+                    <h1 class="titleFirstPart col-12">JEAN</h1>
+                    <h1 class="titleSecPart col-12">FORTEROCHE</h1>
+                    <nav class="col-12">
+                        <div class="row">
+                            <a href="">ACCEUIL</a>
+                            <a href="">BLOG</a>
+                            <a href="">CONTACT</a>
+                        </div>
+                    </nav>
+                    <div class="footerLink col-12">
+                        <div class="row">
+                            <p>2019 Copyright</p>
+                            <a href="">Mention légales</a>
+                            <a href="">Cookies</a>
+                            <a href="">Login Admin</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+
+
+    <!-- CDN => BOOTSTRAP -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    </body>
+    
+</html>
