babca55ea69fd407ece7e22d6c555937a1067522
Lacrotte Florent laptop <lacrotte.florent@gmail.com>
2019-12-30 08:55:46 +0100

    création de page contact

---
+++ contact.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<!-- Couleurs :
+    Blanc       #FFFFFF
+    Gris        #333
+    Gris clair  #919191
+    Gris foncé  #181A1B
+    Orangé      #542E00
+    Orang Clair #F08400
+    Bleu clair  #00A3D0
+    Bleu gris   #38435A
+    Bleu foncé  #181D27
+    -->
+<!-- Blog d'écrivain - Made by Lacrotte Florent / Projet 4 - OpenClassrooms -->
+
+<html>
+    
+    <head>
+    <?php include("includes/include_head.php"); ?>
+    </head>
+    
+    <body>
+        <div class="container">
+
+            <?php include("includes/include_header.php"); ?>
+
+            <div id="contact" class="row">
+                <h2>Contactez moi></h2>
+                <form method="POST" action="contact_post" class="col-12">
+                    <div class="row">
+                        <div class="col 6">
+                            <label for="contactName">Nom : </label>
+                            <input type="text" id="contactName" name="contactName" placeholder="Votre nom" />
+                        </div>
+                        <div class="col-6">
+                            <label for="contactFirstName">Prénom :</label>
+                            <input type="text" id="contactFirstName" name="contactFirstName" placeholder="Votre prénom" />
+                        </div>
+                    </div>
+                    <label for="contactMail">Addresse mail :</label>
+                    <input type="text" id="contactMail" name="contactMail" placeholder="Votre adresse mail" />
+                    <label for="contactObject">Objet du message</label>
+                    <input type="text" id="contactObject" name="contactObject" placeholder="L'objet de votre message" />
+                    <label for="contactMessage">Message</label>
+                    <textarea id="contactMessage" name="contactMessage" cols="30" rows="10">Votre message ici</textarea>
+                    <div class="row">
+                        <div class="col-6">
+                        </div>
+                        <div class="col-6">
+                            <!-- reczptcha-->
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            
+            <?php include("includes/include_footer.php"); ?>
+
+        </div>
+
+    </body>
+    
+</html>
