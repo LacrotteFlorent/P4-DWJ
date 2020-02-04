3011270209599edcef6b93bd71622f771d8890a1
Lacrotte Florent laptop <lacrotte.florent@gmail.com>
2020-02-04 21:55:46 +0100

    routeur ok

--- app/Router/Route.php
+++ app/Router/Route.php
@@ -16,21 +16,27 @@ class Route {
         $this->action = $action;
     }
 
+    //revois une chaine de cractère et retourne cette chaine debarassée de la partie qui se trouve avant la regex
+    public function beforeIndexCleaner($strToClean, $regexForCleanBefore){
+        $strClean  = preg_split($regexForCleanBefore, $strToClean, 0, PREG_SPLIT_OFFSET_CAPTURE);//selectionne tout ce qui se trouve avant et apres index.php
+        $strClean  = $strClean[1]; // garde uniquement la partie de l'url apres index.php (saute le premier element du tableau)
+        $strClean  = $strClean[0]; // supprime le tableau pour garder uniquement l'url restante
+        return $strClean;
+    }
+
     //teste si la route et bonne et récupères les paramètres de la requète
     public function match($requestUri){
-        $path = preg_replace("/:(\w+)/", "([\d]+)", $this->path); // remplace les paramètres par des regexp   preg_replace("recherche", "caractère de remplacement", "sujet à modifier")  [\d] = un chiffre entre 0-9
-        //dump($path);
-        //dump($this->path);
+        $path = preg_replace("/:(\w+)/", "([\d]+)", $this->beforeIndexCleaner($this->path, "/(.index\.php)+/")); // remplace les paramètres par des regexp   preg_replace("recherche", "caractère de remplacement", "sujet à modifier")  [\d] = un chiffre entre 0-9
         $path = str_replace("/","\/", $path); // remplace les / par des / échappés
         dump($path);
         dump($requestUri);
-        if(!preg_match("/^$path$/i", $requestUri, $matches)){  //si notre requete actuelle ne correspond pas a la regexp on retourne false
+        if(!preg_match("/^$path$/i", $this->beforeIndexCleaner($requestUri, "/(.index\.php)+/"), $matches)){  //si notre requete actuelle ne correspond pas a la regexp on retourne false
             echo"FALSE";
             return false;
         }
         //sinon on stocke nos argument dans un tableau
         array_shift($matches); // on saute le premier élément du tableau
-        $this->matches = $matches; // on sauvegarde les pramaètres dans l'instance
+        $this->matches = $matches; // on sauvegarde les paramètres dans l'instance
         dump($matches);
         dump($this->matches);
         return true;
--- app/Router/Router.php
+++ app/Router/Router.php
@@ -14,11 +14,6 @@ class Router {
         $this->url = $this->server["REQUEST_URI"];
     }
 
-    //on vérifie que la route est présente (on teste le param ['action']), s'il est présent on envois la requete vers le controller de la page corrspondante, sinon on renvois vers le controller de la page d'acceuil
-
-    //pour vérifier que la route est présente on apelle la classe route qui vérifie que le chemin existe, s'il existe renvoie vrai sinon renvois faux
-
-
 
     // crée une route pour la méthode $_GET
     public function addRouteGet($path, $controller, $action){
