6f4d410395d5c607ea9aad952959ab43473d2016
Lacrotte Florent laptop <lacrotte.florent@gmail.com>
2020-02-13 14:21:15 +0100

    implementation ORM suite et controller

---
+++ app/ORM/Controller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Framework\ORM;
+
+use Framework\Http\Response;
+use Framework\Http\RedirectionResponse;
+use Framework\Http\Request;
+use Framework\Router\Router;
+
+class Controller
+{
+     /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    /**
+     * BlogController constructor.
+     * @param Request $request
+     * @param Router $router
+     */
+    public function __construct(Request $request, Router $router)
+    {
+        $this->request = $request;
+        $this->router = $router;
+    }
+
+    /**
+     * @param array $paramsMatches
+     * @return Response
+     */
+    public function index($paramsMatches): Response
+    {
+        return new Response("<h1>Reponse</h1>");
+    }
+
+    /**
+     * @param array $paramsMatches
+     * @return RedirectionResponse
+     */
+    public function redirection($paramsMatches): RedirectionResponse
+    {
+        return new RedirectionResponse($this->request->getRequestUri());
+    }
+}
--- src/controller/BlogController.php
+++ src/controller/BlogController.php
@@ -2,50 +2,10 @@
 
 namespace Project\Controller;
 
-use Framework\Http\Response;
-use Framework\Http\RedirectionResponse;
-use Framework\Http\Request;
-use Framework\Router\Router;
+use Framework\ORM\Controller;
 
-class BlogController
+class BlogController extends Controller
 {
-    /**
-     * @var Request
-     */
-    private $request;
-
-    /**
-     * @var Router
-     */
-    private $router;
-
-    /**
-     * BlogController constructor.
-     * @param Request $request
-     * @param Router $router
-     */
-    public function __construct(Request $request, Router $router)
-    {
-        $this->request = $request;
-        $this->router = $router;
-    }
-
-    /**
-     * @param array $paramsMatches
-     * @return Response
-     */
-    public function index($paramsMatches): Response
-    {
-        return new Response("<h1>Blog</h1>");
-    }
-
-    /**
-     * @param array $paramsMatches
-     * @return RedirectionResponse
-     */
-    public function redirection($paramsMatches): RedirectionResponse
-    {
-        return new RedirectionResponse($this->request->getRequestUri());
-    }
+   
 }
 /// La methode d'un controller renvoie toujours une reponse
